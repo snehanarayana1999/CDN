d0b7e3e13f27c5df4ecda76be164c5b108a0f583
snehanarayana1999 <100332135+snehanarayana1999@users.noreply.github.com>
2023-03-24 14:03:42 +0530

    Update sidebar.vue

--- js/vue-components/sidebar.vue
+++ js/vue-components/sidebar.vue
@@ -1,4 +1,5 @@
 <template>
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <body>
         <ul class="sidebar">
         
@@ -14,7 +15,7 @@
             <li v-for="children in item.children" :key="children">
               <h1 v-on:click="children.show = !children.show">
                 {{children.name}}
-                
+             <span v-if="children.next" class="material-symbols-outlined">arrow_drop_down</span>
               </h1>
               <ul class="side-items">
               <ul v-if="children.show">
@@ -38,8 +39,8 @@
     </template>
     
     <script>
-
-import jsonData from '/js/jsonData.json'
+var json = '[{"id": 1,"name": "GENERAL", "show": false, "children": [{ "name": "Cloud Storage","show": false,"next": [{ "name": "Signin"    }, { "name": "Signout" }, { "name": "Logoff" }]}, { "name": "Customers", "show": false, "next": [{  "name": "Signin"}, { "name": "Signout" }, { "name": "Logoff" }]  } ] }, { "id": 2,"name": "ADMINISTRATION","show": false,"children": [{"name": "Team Settings","show": false,"next": [{ "name": "Login"}, { "name": "LogOut" }, { "name": "Logoff" }]}, { "name": "Team Managers" }]},{"id": 3,"name": "INVITATIONS","show": false,"children": [{ "name": "Members","show": false, "next": [{   "name": "Login" }, { "name": "LogOut" }, { "name": "Logoff" }]}, { "name": "Plugin" }] },{"id": 4,"name": "PAYMENTS","show": false,"children": [{ "name": "About" }, { "name": "Password" }] },{"id": 5,"name": "SETTINGS","show": false,"children": [{ "name": "Messages" }, { "name": "Overview" }]}]'
+var jsonData = JSON.parse(json)
 export default{
   data(){
     return{
@@ -48,4 +49,36 @@ export default{
   }
 }
     </script>
+    <style scoped>
+    .sidebar{
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+      margin-top:10px;
+      font-size: 14px;
+      margin-left: 70px; 
+      color:gray;
+      position:absolute;
+      padding:5px;
+      
+    }
+      
+    .side-menu{
+      margin-left: 10px; 
+      color: black;
+      padding: 5px;
+     }
+     
+    .side-menu h1{
+      font-weight:bold;
+      padding: 5px;
+      }
+      
+    .side-menu h1:hover {
+      background-color: transparent;
+      color: #276cda;
+    }
+    .side-items{
+      margin-left: 30px; 
+      }
+      
+    </style>
     
